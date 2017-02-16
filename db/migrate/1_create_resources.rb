3f60fbfa3da1a7da2638e661b1622861e3ca50df
Micah Geisel <micah@botandrose.com>
2017-02-16 12:05:51 -0800

    default values for db columns.

--- db/migrate/1_create_resources.rb
+++ db/migrate/1_create_resources.rb
@@ -2,16 +2,16 @@ class CreateResources < ActiveRecord::Migration[5.0]
   def change
     create_table "resources", primary_key: "resourcesid" do |t|
       t.string   "resourcestype"
-      t.decimal  "resourcesduration",         precision: 10
-      t.integer  "resourceswidth"
-      t.integer  "resourcesheight"
+      t.decimal  "resourcesduration", precision: 10, default: 0
+      t.integer  "resourceswidth", default: 0
+      t.integer  "resourcesheight", default: 0
       t.string   "resourcesfilename"
       t.string   "resourcesoriginalfilename"
       t.string   "resourcespath"
       t.integer  "resourcesthumbid"
       t.string   "resourceshash"
-      t.integer  "resourcesparentid"
-      t.integer  "resourcesparentindex"
+      t.integer  "resourcesparentid", default: 0
+      t.integer  "resourcesparentindex", default: 0
       t.datetime "created_at",                               null: false
       t.datetime "updated_at",                               null: false
     end
